81c2d4454c30f5808f8831758f6d46fb52d7591b
testluoshiling <15901304864@163.com>
2019-12-11 11:11:30 +0800

    第一次迁移

---
+++ apimadetest/urls.py
@@ -0,0 +1,33 @@
+"""apimadetest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url
+from apitest.views import *
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls, ),
+    url(r'^api_cancel_order/', cancel_order),
+    url(r'^api_payback_order/', payback_order),
+    url(r'^api_others_pay_order/', others_pay_order),
+    url(r'^api_pay_order/', pay_order),
+    url(r'^api_add_order/', create_order),
+    url(r'^api_add_lodgeinfo/', add_lodgeinfo),
+    url(r'^api_register/', register),
+    url(r'^api_login_get', login_get),
+    url(r'^api_login_post/', login_post),
+    url(r'^api_demo/', api_demo),
+    url(r'^', welcome),
+]
---
+++ apitest/test/tests.py
@@ -0,0 +1,113 @@
+from django.test import TestCase
+import requests
+import json
+#from apitest.models import *
+# Create your tests here.
+
+def login(username, password):
+    url = 'http://127.0.0.1:8000/api_login_post/'
+    data = {
+        'username': username,
+        'password': password
+    }
+    json.dumps(data)
+    re = requests.post(url, json=data)
+    print("登录信息", re.text)
+
+def login1():
+    url = 'http://127.0.0.1:8000/api_login_get?username=luoshiling&password=admin12345'
+    re = requests.get(url)
+    print(re.text)
+
+
+
+def register(username, password, email):
+    url = 'http://127.0.0.1:8000/api_register/'
+    data = {
+        'username': username,
+        'password': password,
+        'email': email,
+    }
+    json.dumps(data)
+    re = requests.post(url, json=data)
+    print("注册信息",re.text)
+
+
+def add_lodgeInfo(info_dict):
+    url = 'http://127.0.0.1:8000/api_add_lodgeinfo/'
+    json.dumps(info_dict)
+    re = requests.post(url,json=info_dict)
+    print("添加房源", re.text)
+
+
+
+def add_order(order_info):
+    url = 'http://127.0.0.1:8000/api_add_order/'
+    json.dumps(order_info)
+    re = requests.post(url, json=order_info)
+    print(re.text)
+
+def pay_order(pay_order_info):
+    url = 'http://127.0.0.1:8000/api_pay_order/'
+    json.dumps(pay_order_info)
+    re = requests.post(url, json=pay_order_info)
+    print(re.text)
+
+def others_pay_order(others_pay_order_info):
+    url = 'http://127.0.0.1:8000/api_others_pay_order/'
+    json.dumps(others_pay_order_info)
+    re = requests.post(url, json=others_pay_order_info)
+    print(re.text)
+
+
+def order_cancel(cancel_info):
+    url = "http://127.0.0.1:8000/api_cancel_order/"
+    json.dumps(cancel_info)
+    re = requests.post(url, json=cancel_info)
+    print(re.text)
+
+
+
+
+if __name__ == '__main__':
+
+    # username = "luoshiling20",
+    # password = "admin12345",
+    # email = "15901304866@163.com"
+    # register(username, password, email)
+    # login(username, password)
+    # info_dict = {"dayprice":3,
+    #              "estate":"valid",
+    #              "minday":1,
+    #              "maxday":2,
+    #              "tel":"15901304864",
+    #              "remarks":"",
+    #              "address_id":"124253424342",
+    #              "image_md5":"sfdgwet4husf98fwiuhfsjkdhwh"
+    #             }
+    # add_lodgeInfo(info_dict)
+    order_info = {
+        "luid":1,
+        "guestnum":2,
+        "checkinday":"2019-01-03",
+        "checkoutday":"2019-01-04"
+    }
+    add_order(order_info)
+    # pay_order_info = {
+    #     "order_id": 1,
+    #     "luid": 1
+    # }
+    # pay_order(pay_order_info)
+    # others_pay_order_info = {
+    #      "order_id": 5,
+    #      "totalprice": 3
+    # }
+    # others_pay_order(others_pay_order_info)
+    cancel_info = {
+        "order_id": 10
+    }
+    order_cancel(cancel_info)
+
+
+
+
